5f63d0f74893b8a531381422b768d48914ccffb4
Parth Jariwala <19jariwalap@gmail.com>
2024-08-20 15:56:46 -0400

    different functions exercises

---
+++ R/functions.R
@@ -0,0 +1,12 @@
+# start out with a number to test
+x <- 3
+# you'll want your function to return this number
+x^2
+square <- function(x) {
+
+
+}
+# test it out
+square(x)
+square(53)
+53^2 # does this match?
