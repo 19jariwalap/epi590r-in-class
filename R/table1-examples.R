500b106aad60f9501accdb06f667675401032684
Parth Jariwala <19jariwalap@gmail.com>
2024-08-20 10:38:01 -0400

    table1

--- R/table1-examples.R
+++ R/table1-examples.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(gtsummary)
+library(dplyr)
 
 nlsy_cols <- c("glasses", "eyesight", "sleep_wkdy", "sleep_wknd",
 							 "id", "nsibs", "samp", "race_eth", "sex", "region",
@@ -56,4 +57,37 @@ tbl_summary(
 	bold_labels() |>
 	modify_footnote(update = everything() ~ NA) |>
 	modify_header(label = "**Variable**", p.value = "**P**")
+
+
+# Exercise
+
+tbl_summary(
+	nlsy,
+	by = sex_cat,
+	digits = list(income ~ 3,
+								sleep_wkdy ~ 1,
+								sleep_wknd ~1),
+	include = c(region_cat, sex_cat, race_eth_cat, income,
+						 sleep_wkdy, sleep_wknd),
+	label = list(
+		region_cat ~ "Region",
+		race_eth_cat ~ "Race/ethnicity",
+		income ~ "Income",
+		sleep_wkdy ~ "Hours slept on Weekday",
+		sleep_wknd ~ "Hours slept on Weekend"
+	),
+	missing_text = "Missing") |>
+	add_p(test = list(all_continuous() ~ "t.test",
+										all_categorical() ~ "chisq.test")) |>
+	add_overall(col_label = "**Total**") |>
+	bold_labels() |>
+	modify_footnote() |>
+	modify_table_styling(columns = label,
+											 rows = label == "Race/ethnicity",
+											 footnote = "How NLSY classified participants: https://www.nlsinfo.org/content/cohorts/nlsy79/topical-guide/household/race-ethnicity-immigration-data"
+	) %>%
+	modify_header(label = "**Variable**", p.value = "**P**") %>%
+	list(income ~ "(p{10}, p{90})",
+				sleep_wkdy ~ "min = {min}; max ={max}",
+				sleep_wknd ~ "min = {min}; max ={max}")
 
